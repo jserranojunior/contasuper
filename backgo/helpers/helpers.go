b5c2cdc7fd4c7e9f796fab393feb205d647d7759
Jorge S junior <jorgeserranojunior@gmail.com>
2026-09-15 10:07:48 -0300

    helpers

--- backgo/helpers/helpers.go
+++ backgo/helpers/helpers.go
@@ -6,6 +6,7 @@ import ("strconv"
 func StringToUint(value string)uint{
 			intValue, err := strconv.Atoi(value)
 					if err != nil{
+						fmt.Println("Erro ao converter string para int:", err)
 						fmt.Println(err)
 					}
 						uintValue := uint(intValue)
